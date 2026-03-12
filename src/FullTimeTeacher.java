be167b9188e6841bdfc18d651352acd6621bf167
SantiagoBelalcazarG <santiago.belalcazar@globant.com>
2026-03-12 09:28:08 -0500

    feat: add abstract teacher model and subclasses

---
+++ src/FullTimeTeacher.java
@@ -0,0 +1,31 @@
+public class FullTimeTeacher extends Teacher{
+
+    private int experience;
+
+    public FullTimeTeacher(String name, double salary, int experience) {
+        super(name, salary);
+        this.experience = experience;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    @Override
+    public double calculateSalary(){
+        return getSalary() * (getExperience() * 1.1);
+    }
+
+    @Override
+    public String toString() {
+        return "Profesor de Tiempo Completo: " +
+                "nombre = " + getName() + " " +
+                "salario = $" + calculateSalary() + " " +
+                "experiencia = " + experience;
+    }
+
+}
---
+++ src/PartTimeTeacher.java
@@ -0,0 +1,31 @@
+public class PartTimeTeacher extends Teacher{
+
+    private int activeHours;
+
+    public PartTimeTeacher(String name, double salary, int activeHours) {
+        super(name, salary);
+        this.activeHours = activeHours;
+    }
+
+    public int getActiveHours() {
+        return activeHours;
+    }
+
+    public void setActiveHours(int activeHours) {
+        this.activeHours = activeHours;
+    }
+
+    @Override
+    public double calculateSalary(){
+        return getSalary() * getActiveHours();
+    }
+
+    @Override
+    public String toString() {
+        return "Profesor de Tiempo Parcial:  " +
+                "nombre = " + getName() + " " +
+                "salario = $" + calculateSalary() + " " +
+                "horas semanales = " + activeHours;
+    }
+    
+}
